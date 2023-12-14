a2cef0a47fd3aa46bcb3f32a76db22e3653441ab
Maxime Daraiche <maxiscoding@gmail.com>
2023-12-14 18:42:16 -0500

    Added bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Simple Bootstrap script to install my dotfiles
+
+current_dir=$(pwd)
+
+home_dotfiles=(
+    .bashrc
+    .emacs
+    .gitconfig
+    .guile
+    .tmux.conf
+    .vimrc
+)
+
+# Symlink our dotfiles
+for dotfile in "${home_dotfiles[@]}"; do
+    echo "> Symlinking $dotfile to HOME directory."
+    ln -sf "$current_dir/$dotfile" $HOME/$dotfile
+done
+
+# Place various configurations where they're supposed to be 
+mkdir -p "$HOME/.gnupg" && cp gpg-agent.conf "$HOME/.gnupg/"
