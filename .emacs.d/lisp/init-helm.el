c8bff4a0bd3f515db4b6a166ed077d6d7143ae0d
Maxime Daraiche <maxiscoding@gmail.com>
2024-01-15 08:19:28 -0500

    Removed .emacs.d and create its repository

--- .emacs.d/lisp/init-helm.el
+++
@@ -1,6 +0,0 @@
-(use-package helm
-  :ensure t
-  :bind
-  (("C-x C-f" . helm-find-files)))
-
-(provide 'init-helm)
--- .emacs.d/lisp/init-key-chord.el
+++
@@ -1,5 +0,0 @@
-(use-package key-chord
-  :config
-  (key-chord-mode 1))           
-  ;;(key-chord-define evil-insert-state-map "jk" 'evil-normal-state))
-(provide 'init-key-chord)
